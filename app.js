33a2b9af663b052a689842ac1640779937cc41e4
rohan <rohan.t@scriptbees.com>
2018-12-04 11:58:56 +0530

    all

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 console.log('hello world')
 
 /*modals inherited*/
-var express = require('express')
+const express = require('express')
+const app = express()
+const port = 3000
+
 var bosyParser = require('body-parser')
 var path = require('path')
-var app = express()
+
 /*initiating the application*/
 app.listen(3000,function(){
 console.log('server stated on port 3000')
 })
 
 /*handle url*/
-app.get('/', function(req,res){
-	console.log('homepage')
-	res.send('hello world')
-})
+app.get('/', (req,res) => {console.log('homepage'); res.send('hello worlda')} )
 
+// app.get('/', (req, res) => res.send('Hello World!'))
 
 /*creating a middleware*/
 var logger = function(req, res, next) {
 	console.log('logged...')
 	next()
 	}
+
+
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
